6e683f52ff2b8bb41d5ff7e4c3f3cbc260f2d4cd
Chuancong Gao <chuanconggao@users.noreply.github.com>
2018-05-05 14:23:50 -0700

    Optimize index and refactoring

--- setup.py
+++ setup.py
@@ -37,7 +37,6 @@ setup(
 
     python_requires=">= 3",
     install_requires=[
-        "docopt >= 0.6.2",
-        "extratools >= 0.3.5"
+        line.strip() for line in open("requirements.txt")
     ]
 )
